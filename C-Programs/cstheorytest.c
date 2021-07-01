d2ae42bfb9fb875f189437d20f1bb40275cab485
Pranay Gupta <pranayguptamain@gmail.com>
2021-07-01 10:52:56 +0530

    update

--- C-Programs/cstheorytest.c
+++ C-Programs/cstheorytest.c
@@ -1,9 +1,15 @@
-//03_PRANAY GUPTA
 #include <stdio.h>
-//#include <string.h>
-//#include <math.h>
+// #include <string.h>
+#include <math.h>
 
 int main()
 {
-    
+    if(-5)
+    {
+        printf("A");
+    }
+    if(5)
+        printf("E");
+        printf("C");
+    return 0;
 }
