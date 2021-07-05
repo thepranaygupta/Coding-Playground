2cdb016b298003bc6bb206b0caf7026146ca8649
Pranay Gupta <pranayguptamain@gmail.com>
2021-07-05 20:20:49 +0530

    new

---
+++ C-Programs/areaandperimeterofcircle.c
@@ -0,0 +1,39 @@
+//03_PRANAY GUPTA
+#include <stdio.h>
+
+// -------------------CALL BY ADDRESS--------------------
+// void areaperi(double *r, double *area, double *peri)
+// {
+//     *area = 3.14 * *r * *r;
+//     *peri = 2 * 3.14 * *r;
+// }
+// int main()
+// {
+//     double r, area, peri;
+//     printf("Enter the Radius: ");
+//     scanf("%lf", &r);
+//     areaperi(&r, &area, &peri);
+//     printf("Radius = %lf", r);
+//     printf("\nArea = %lf", area);
+//     printf("\nPerimeter = %lf", peri);
+//     return 0;
+// }
+
+// -------------------CALL BY VALUE-----------------------
+double area(double r)
+{
+    return (3.14 * r *r);
+}
+double perimeter(double r)
+{
+    return(2 * 3.14 * r);
+}
+int main()
+{
+    double r;
+    printf("Enter the Radius: ");
+    scanf("%lf", &r);
+    printf("Radius = %lf", r);
+    printf("\nArea = %lf", area(r));
+    printf("\nPerimeter = %lf", perimeter(r));
+}
